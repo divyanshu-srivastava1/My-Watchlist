15596af51613d23e7efe45131112310960259c06
Divyanshu Srivastava <61875884+divyanshu-srivastava1@users.noreply.github.com>
2021-09-18 14:40:39 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,23 +7,7 @@ import './index.css';
 import App from './components/App';
 import rootReducer from './reducers';
 
-// const logger = function({dispatch, getState}){
-//   return function(next){
-//     return function(action){
-//       console.log(action.type)
-//       next(action)
-//     }
-//   }
-// }
-
-// const logger = ({dispatch, getState})=>(next)=>(action)=>{
-//   console.log(action.type)
-//   next(action)
-// }
-
 const store = createStore(rootReducer, applyMiddleware(thunk));
-//console.log(store);
-
 
 ReactDOM.render(
   <React.StrictMode>
